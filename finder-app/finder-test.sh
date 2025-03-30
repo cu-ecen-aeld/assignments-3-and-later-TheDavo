ba67c52d5158b6d4281667c313debaaea55eada8
Davit Khudaverdyan <davit.khudaverdyan@colorado.edu>
2025-03-30 11:07:37 -0400

    Update for assignment 4 part 2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Tester script for assignment 1 and assignment 2
 # Author: Siddhant Jajoo
+# Modified by Davit Khudaverdyan for AELD Homework
 
 set -e
 set -u
@@ -8,7 +9,15 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+# username=$(cat conf/username.txt)
+# for homework 4 assignment 2:
+username=$(can /etc/finder-app/conf/username.txt)
+ASSIGNMENT4_FILE=/tmp/assignment4-result.txt
+
+# change user directory to where ./finder-test.sh is so that the later
+# ./finder.sh call works
+FINDER_DIR=$(dirname $0)
+cd ${FINDER_DIR}
 
 if [ $# -lt 3 ]
 then
@@ -32,7 +41,9 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat ./conf/assignment.txt`
+# assignment=`cat ./conf/assignment.txt`
+# for homework 4 assignment 2:
+assignment=`cat /etc/finder-app/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -58,6 +69,8 @@ do
 done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+# for homework 4 assignment 2:
+echo OUTPUTSTRING > ${ASSIGNMENT4_FILE}
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
