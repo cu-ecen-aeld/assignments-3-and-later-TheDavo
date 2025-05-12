13ebba8d5df631e46641fbb7459636d98faa58f8
Davit Khudaverdyan <davit.khudaverdyan@colorado.edu>
2025-05-11 22:37:35 -0400

    Add the start stop positional argument to the script

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-start-stop-daemon -S --name aesdsocket -x "/usr/bin/aesdsocket" -- -d
+case "$1" in
+  start)
+    echo "Starting aesdsocket"
+    start-stop-daemon -S --name aesdsocket -x "/usr/bin/aesdsocket" -- -d
+    ;;
+  stop)
+    echo "Stopping aesdocket"
+    start-stop-daemon -K -n aesdsocket
+    ;;
+  *)
+    echo "Usage: $0 {start|stop}"
+    exit 1
+    ;;
+
+esac
+
+exit 0
